d8a9de8865ad7fad480ea681def374c316071926
Josue049 <josue09guti@gmail.com>
2026-05-31 19:07:29 -0500

    feat/ settings and session

--- app/db.py
+++ app/db.py
@@ -10,6 +10,12 @@ from sqlalchemy.orm import Session, sessionmaker
 
 BASE_DIR = Path(__file__).resolve().parents[1]
 
+settings = Settings()
+
+engine = create_engine(settings.database_url, **engine_kwargs)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, expire_on_commit=False, future=True)
+
+
 def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
